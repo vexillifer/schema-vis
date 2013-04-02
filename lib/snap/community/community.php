f5ea0ccb4c2e70975dadfe4c6ae4782081fecc38
Peter Beshai <peter.beshai@gmail.com>
2013-04-02 14:48:54 -0700

    Fix community bug

--- lib/snap/community/community.php
+++ lib/snap/community/community.php
@@ -1,72 +1,75 @@
 <?php
 
-if(isset($_POST['edges'])) {
-    $edges = $_POST['edges'];
+if(!isset($_POST['edges'])) {
+die("[]");
+}
 
-    $hash = md5($edges);
-    $cached_file = "cache/comm_output_".$hash.".txt";
-    $fh = null;
+$edges = $_POST['edges'];
 
-    // check for cached solution
-    if(file_exists($cached_file)) {
-
-        $fh = fopen($cached_file, 'r');
-    } else {
-
-        // write edges to graph format and run SNAP community detection
-        // NOTE: we need write permissinos on comm_input and comm_output
-        // otherwise we get really annoying and hard to find errors.
-        $comm_input = "comm_input.txt";
-        $comm_output = "comm_output.txt";
-        $fh = fopen($comm_input, 'w');
-        fwrite($fh, $edges);
-        fclose($fh);
-        $output = array();
-        $return_val = null;
-        exec("./community -i:$comm_input -o:$comm_output -a:1 2>&1", $output, $return_val);
-
-        // cache the result
-        copy($comm_output, $cached_file);
-
-        // Load output and parse results
-        $fh = fopen($comm_output, 'r');
-    }
+if(empty($edges)) {
+    die("[]");
+}
 
 
-    $cluster = array();
-    $clusters = array();
-    $cluster_num = 0;
+$hash = md5($edges);
+$cached_file = "cache/comm_output_".$hash.".txt";
+$fh = null;
 
-    // Create an array of clusters (an array of node indices)
-    while (!feof($fh)) {
-        $line = fgets($fh);
-        // Filter out meta data
-        if (preg_match("/#/", $line) === 0) {
-            $parts = preg_split('/\s/', $line);
-            if (count($parts) > 1 && $parts[1] === strval($cluster_num)) {
-                $cluster[] = $parts[0];
-            } else {
-                // begin next cluster
-                $clusters[] = $cluster;
-                $cluster = array();
-                $cluster_num++;
-                $cluster[] = $parts[0];
-            }
+// check for cached solution
+if(file_exists($cached_file)) {
+
+    $fh = fopen($cached_file, 'r');
+} else {
+
+    // write edges to graph format and run SNAP community detection
+    // NOTE: we need write permissinos on comm_input and comm_output
+    // otherwise we get really annoying and hard to find errors.
+    $comm_input = "comm_input.txt";
+    $comm_output = "comm_output.txt";
+    $fh = fopen($comm_input, 'w');
+    fwrite($fh, $edges);
+    fclose($fh);
+    $output = array();
+    $return_val = null;
+    exec("./community -i:$comm_input -o:$comm_output -a:1 2>&1", $output, $return_val);
+
+    // cache the result
+    copy($comm_output, $cached_file);
+
+    // Load output and parse results
+    $fh = fopen($comm_output, 'r');
+}
+
+
+$cluster = array();
+$clusters = array();
+$cluster_num = 0;
+
+// Create an array of clusters (an array of node indices)
+while (!feof($fh)) {
+    $line = fgets($fh);
+    // Filter out meta data
+    if (preg_match("/#/", $line) === 0) {
+        $parts = preg_split('/\s/', $line);
+        if (count($parts) > 1 && $parts[1] === strval($cluster_num)) {
+            $cluster[] = $parts[0];
+        } else {
+            // begin next cluster
+            $clusters[] = $cluster;
+            $cluster = array();
+            $cluster_num++;
+            $cluster[] = $parts[0];
         }
     }
-
-    // Formatting to [[],[],[]]
-    $cluster_str = '';
-    foreach($clusters as $cluster) {
-        $cluster_str .= '[' . implode(",", $cluster) . '],';
-    }
-    $cluster_str = '[' . rtrim($cluster_str, ',') . ']';
-
-    fclose($fh);
-    echo $cluster_str;
-
-} else {
-    // no edges passed, no clusters returned
-    echo "[]";
 }
+
+// Formatting to [[],[],[]]
+$cluster_str = '';
+foreach($clusters as $cluster) {
+    $cluster_str .= '[' . implode(",", $cluster) . '],';
+}
+$cluster_str = '[' . rtrim($cluster_str, ',') . ']';
+
+fclose($fh);
+echo $cluster_str;
 ?>
