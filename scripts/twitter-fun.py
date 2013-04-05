36e1ee11c4bf2628bb7e0fa410aff23ebae6c754
Kalan MacRow <kalanwm@cs.ubc.ca>
2013-04-04 18:50:21 -0700

    max seed size, utf-8 output for azn chars

--- scripts/twitter-fun.py
+++ scripts/twitter-fun.py
@@ -13,6 +13,10 @@ def safe(str):
 def screen_name(sc):
     return hashlib.md5(sc).hexdigest()[:8]
 
+# Only grab COUNT_MAX friends of seed user
+count = 0
+COUNT_MAX = 50
+
 # Instantiate the API
 api = twitter.Api(       consumer_key='jB7yF8qSUUX1XQQrnecQ',
                       consumer_secret='vVpf89fdsPZ5sBk65Fvw6uXSYQ4SrU5GrVO1SHw',
@@ -20,8 +24,6 @@ api = twitter.Api(       consumer_key='jB7yF8qSUUX1XQQrnecQ',
                   access_token_secret='p7zBA0saOngXD0GPyRhnDBHIlZNYdJDsGmKHYywu4')
 
 # In memory data (we'll output XML later)
-count = 0
-count_max = 50
 people = {}
 connections = []
 
@@ -38,18 +40,20 @@ people[root.name] = root
 # Get mutual friends network for single person
 my_followers = api.GetFollowers(user=root)
 for my_follower in my_followers:
-  
-  count = count + 1
-  if count > count_max:
-    break
 
   people[my_follower.name] = my_follower
   connections.append([root.name, my_follower.name])
+
+  count = count + 1
+  if count > COUNT_MAX:
+    break
+
   # Don't crawl crazy ass accounts (@ILoveBurritos, etc.)
   if my_follower.followers_count is not None and my_follower.followers_count < 300:
     try:
       followers = api.GetFollowers(user=my_follower)
       for follower in followers:
+        
         people[follower.name] = follower
         connections.append([my_follower.name, follower.name])
     except:
@@ -57,38 +61,36 @@ for my_follower in my_followers:
       pass
 
 # Some stats (delete this from XML)  
-print '     people: %d' % len(people.keys())
-print 'connections: %d' % len(connections)
+print u'     people: %d' % len(people.keys())
+print u'connections: %d' % len(connections)
 print
 
 # dump it to XML
-print '<?xml version="1.0" encoding="UTF-8"?>'
-print '<network>'
+print u'<?xml version="1.0" encoding="UTF-8"?>'
+print u'<network>'
 
 for name in people:
-  person = people[name]
-  print ' <person>'
-  try:
-    print '   <name>%s</name>' % safe(person.name)
-    print '   <locale>%s</locale>' % safe(person.location)
-    print '   <lang>%s</lang>' % safe(person.lang)
-    print '   <screen>%s</screen>' % safe(person.screen_name)
-    print '   <created>%s</created>' % safe(person.created_at)
-  except:
-    # encoding woes...
-    pass
+  p = people[name]
+  print u' <person>'
+  if hasattr(p, 'name') and p.name != None:
+    print (u'   <name>%s</name>' % p.name).encode('utf-8')
+  if hasattr(p, 'location') and p.location != None:
+    print (u'   <location>%s</location>' % p.location).encode('utf-8')
+  if hasattr(p, 'lang') and p.lang != None:
+    print (u'   <lang>%s</lang>' % p.lang).encode('utf-8')
+  if hasattr(p, 'screen') and p.screen != None:
+    print (u'   <screen>%s</screen>' % p.screen_name).encode('utf-8')
+  if hasattr(p, 'created_at') and p.created_at != None:
+    print (u'   <created>%s</created>' % p.created_at).encode('utf-8')
   print ' </person>'
 
 for connection in connections:
-  print ' <connection>'
-  try:
-    print '   <uid1>%s</uid1>' % safe(connection[0])
-    print '   <uid2>%s</uid2>' % safe(connection[1])
-  except:
-    pass
-  print ' </connection>'
+  print u' <connection>'
+  print (u'   <uid1>%s</uid1>' % connection[0]).encode('utf-8')
+  print (u'   <uid2>%s</uid2>' % connection[1]).encode('utf-8')
+  print u' </connection>'
 
-print '</network>'
+print u'</network>'
 
 
 
